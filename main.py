a4d2f9426bb6c1cb7d7ddb6f11687736f06eee14
jbarneyy <js_barnett@outlook.com>
2025-06-17 11:48:47 -0700

    Added types, will use to send chat history to Gemini. Added calculator files to calculator dir

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ from dotenv import load_dotenv
 
 # Imports the genai module from Google's SDK (google-generativeai). Access to the Client and model methods needed to talk to Gemini.
 from google import genai
+from google.genai import types
 
 def main():
 
-    # Reads the .env file in the root of your project. Loads the variables into the environment.
+    if (len(sys.argv) < 2):
+        print("Need to specify prompt for Gemini!")
+        exit(1)
+
+    # Reads the .env file in the root of project. Loads the variables into the environment.
     load_dotenv()
 
     # Retrieves the value of GEMINI_API_KEY from the environment.
@@ -22,13 +27,30 @@ def main():
     # Grabs second value in sys.argv to be used as content to be fed to Gemini for response.
     content = sys.argv[1]
 
-    # Calls Gemini’s generate_content() method using the "gemini-2.0-flash-001" model.
-    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = content)
+    # Creating list named messages with a single Content object. Content object includes a nested Part object with message text.
+    messages = [types.Content(role="user", parts=[types.Part(text=content)])]
 
-    # Print out response's text, prompt token count, and response token count.
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # Calls Gemini’s generate_content() method using the "gemini-2.0-flash-001" model.
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+    # Print out response's text, prompt token count, and response token count. If --verbose flag included.
+    if is_verbose():
+        print(response.text)
+        print(f"User prompt: {content}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(response.text)
+
+
+def is_verbose():
+    if (len(sys.argv) > 2):
+        return sys.argv[2] == "--verbose"
+    else:
+        return False
+
+
+
 
 
 
