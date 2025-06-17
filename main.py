2557b73c509a011d55793ed20a73e4e783a1eef0
jbarneyy <js_barnett@outlook.com>
2025-06-16 17:10:26 -0700

    Added main.py, .gitignore, and .env / requirements.txt files. Created basic structure for API requests from Google Gemini.

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model = "gemini-2.0-flash-001", contents = content)
+
+    print(response.text)
+
+
+main()
