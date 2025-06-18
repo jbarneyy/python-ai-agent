156883f50d5ab4c2f6fb197bbb43e21d309d3aa0
jbarneyy <js_barnett@outlook.com>
2025-06-17 17:56:27 -0700

    Added function to get info of all files/directories in given directory

---
+++ functions/get_files_info.py
@@ -0,0 +1,34 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+
+    working_dir_path = os.path.abspath(working_directory)
+    dir_path = os.path.join(working_dir_path, directory)
+    dir_path = os.path.abspath(dir_path)
+
+    # print(working_dir_path)
+    # print(dir_path)
+
+    if (not dir_path.startswith(working_dir_path)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if (not os.path.isdir(dir_path)):
+        return f'Error: "{directory}" is not a directory'
+
+    dir_contents = os.listdir(dir_path)
+    # print(dir_contents)
+
+    dir_item_info = []
+
+    for item in dir_contents:
+        item_path = os.path.join(dir_path, item)
+        # print(item_path)
+
+        dir_item_info.append(
+            f"- {item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}"
+        )
+    
+    # print(dir_item_info)
+
+
+    return "\n".join(dir_item_info)
---
+++ tests.py
@@ -0,0 +1,12 @@
+from functions.get_files_info import get_files_info
+
+print(get_files_info("calculator", "."))
+print("\n")
+
+print(get_files_info("calculator", "pkg"))
+print("\n")
+
+print(get_files_info("calculator", "/bin"))
+print("\n")
+
+print(get_files_info("calculator", "../"))
