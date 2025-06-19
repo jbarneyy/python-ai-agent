1bdbc3af7fba054b6029ffb5dbf86a804cd2d938
jbarneyy <js_barnett@outlook.com>
2025-06-18 18:13:23 -0700

    Renamed run_python to run_python_file. Added schema function declarations to pass to Gemini to choose which function to use. Updated system prompt for Gemini.

--- main.py
+++ main.py
@@ -27,11 +27,25 @@ def main():
     # Grabs second value in sys.argv to be used as content to be fed to Gemini for response.
     content = sys.argv[1]
 
+    system_prompt = """
+        You are a helpful AI coding agent.
+        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+        - List files and directories
+        - Read file contents
+        - Execute Python files with optional arguments
+        - Write or overwrite files
+        All paths you provide should be relative to the working directory. 
+        You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+        """
+    
     # Creating list named messages with a single Content object. Content object includes a nested Part object with message text.
     messages = [types.Content(role="user", parts=[types.Part(text=content)])]
 
     # Calls Gemini’s generate_content() method using the "gemini-2.0-flash-001" model.
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", 
+                                              contents=messages, 
+                                              config=types.GenerateContentConfig(tools=[available_functions],
+                                                                                 system_instruction=system_prompt))
 
     # Print out response's text, prompt token count, and response token count. If --verbose flag included.
     if is_verbose():
@@ -40,6 +54,9 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
+        if (response.function_calls):
+            for function_call in response.function_calls:
+                print(f"Calling function: {function_call.name}({function_call.args})")
         print(response.text)
 
 
@@ -50,8 +67,79 @@ def is_verbose():
         return False
 
 
+# Summary of our get_files_info function that we can pass to LLM with descriptors.
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+# Summary of our get_file_content function that we can pass to LLM with descriptors.
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads a file, returns a string containing all of a file's text, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file that we will be returning the contents of, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+# Summary of our run_python function that we can pass to LLM with descriptors.
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a Python file with optional arguments, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file that we will be executing, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+# Summary of our write_file function that we can pass to LLM with descriptors.
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes or overwrites a file, if file does not exist will create a file. Constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file that we will be writing, overwriting, or creating. Relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content, message, or text that we will be writing to the designated file.")
+        },
+    ),
+)
 
 
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
+    ]
+)
 
 
+# Entry point for our main function.
 main()
